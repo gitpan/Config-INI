8d1864ff4afc7a710093a05325358b0d2e1192c7
Ricardo SIGNES <rjbs@cpan.org>
2007-11-22 02:40:53 +0000

    Import of RJBS/Config-INI-0.011 from CPAN.
    gitpan-cpan-distribution: Config-INI
    gitpan-cpan-version:      0.011
    gitpan-cpan-path:         RJBS/Config-INI-0.011.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Config/INI.pm
+++ lib/Config/INI.pm
@@ -9,11 +9,11 @@ Config::INI - simple .ini-file format
 
 =head1 VERSION
 
-version 0.010
+version 0.011
 
 =cut
 
-$Config::INI::VERSION = '0.010';
+$Config::INI::VERSION = '0.011';
 
 =head1 GRAMMAR
 
@@ -43,7 +43,7 @@ not-quite-standardized "INI" format.
 
   space = ( <TAB> | " " ) *
   string-without-equals = string - "="
-  string                = ? a string of characters, excluding ; or EOL ?
+  string = ? 1+ characters; not ";" or EOL; begins and ends with non-space ?
 
 Of special note is the fact that I<no> escaping mechanism is defined, meaning
 that there is no way to include an EOL or semicolon (for example) in a value,
